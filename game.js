ff37e2e40120104cfa8f74891863fdb65071bff2
Logan <ldlope30@harriosnburg.k12.va.us>
2019-09-17 13:34:25 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,14 +1,19 @@
 var score = 0;
-
+var score1 = 0;
 postToPage(score); // Print to the page
 
-createButton('Button', btnPress); // Make a button
+createButton('Dont click me', btnPress); // Make a button
 changeTitle('Clicker Game');
-
+createButton('click me', btnPressed);
 setInterval(btnPress, 1000);
 
 function btnPress() {
-  score++;
+  score = score*2;
+
   postToPage(score);
 }
+function btnPressed() {
+  score++;
 
+  postToPage(score);
+}
