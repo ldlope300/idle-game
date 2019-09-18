cb5c7e10642405ae0d2a1b85d4e6c6668742194c
Logan <ldlope30@harriosnburg.k12.va.us>
2019-09-18 14:31:39 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 var score = 1; // Create a variable to store the score in
-
+var hey = new Button('Good Luck', btn)
 var title = new Title('Welcome to the Game'); // Create a title element
 var button = new Button('hello', btnPress);   // Create a button element
 var hidden = new Button('Boo!');
@@ -14,7 +14,15 @@ function btnPress() {
   if(score > 10) {
     hidden.show();
   }
+}
+
+  function btn() {
+    score = score-1;         // Increase the score
+    scr.edit(score); // Update the page with the new score
   if(score > 15) {
     hidden.hide(500);
   }
 }
+ if(score = 1000){
+   
+ }
