8bd11b2b04990d92c8deb124be173b89ac88a311
Logan <ldlope30@harriosnburg.k12.va.us>
2019-09-19 14:33:36 -0400

    Update game.js

--- game.js
+++ game.js
@@ -2,17 +2,17 @@ var score = 1; // Create a variable to store the score in
 var hey = new Button('Good Luck', btn)
 var title = new Title('Welcome to the Game'); // Create a title element
 var button = new Button('hello', btnPress);   // Create a button element
-var hidden = new Button('Boo!');
-hidden.hide();
-var scr = new Text(score);                    // Create a text element
+var hidden = new Button('Boo!', booHide);
+//hidden.hide();
+var gather = 1                  // Create a text element
 
 setInterval(btnPress, 1000);   // Set up a loop
-
-function btnPress() {
+setInterval(gather)
+function btnPress(gather, 10000) {
   score++;         // Increase the score
   scr.edit(score); // Update the page with the new score
   if(score > 10) {
-    hidden.show();
+    //hidden.show();
   }
 }
 
@@ -22,7 +22,19 @@ function btnPress() {
   if(score > 15) {
     hidden.hide(500);
   }
+
 }
  if(score = 1000){
-   
+
+ }
+if(gather > 100){
+  hidden.show(;)
+}
+
+ function booHide() {
+   hidden.hide(1);
+
+   if(gather > 100){
+     hidden.show()
+   }
  }
