b80d94968a0c6ffdb3413169defd8ffa36338ed6
Logan <ldlope30@harriosnburg.k12.va.us>
2019-09-27 13:32:04 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,24 +1,19 @@
 var score = 1; // Create a variable to store the score in
 var hey = new Button('Good Luck', btn)
-var title = new Title('Welcome to the Game'); // Create a title element
-var button = new Button('hello', btnPress);   // Create a button element HEAD
-var hidden = new Button('Boo!', booHide);
+var title = new Title('Welcome to the worlds most famous bakery in the whole world!!'); // Create a title element
+var button = new Button('', btnPress);   // Create a button element HEAD
+var hidden = new Button('Gather dough', booHide);
 //hidden.hide();
 var gather = 1                  // Create a text element
-
-=======
-var hidden = new Button('Boo!');
-//hidden.hide();
-var scr = new Text(score);                    // Create a text element
+             // Create a text element
 //changes
->>>>>>> upstream/master
 setInterval(btnPress, 1000);   // Set up a loop
-setInterval(gather)
-function btnPress(gather, 10000) {
+//setInterval()
+function btnPress() {
   score++;         // Increase the score
   scr.edit(score); // Update the page with the new score
   if(score > 10) {
-    //hidden.show();
+    hidden.show();
   }
 }
 
@@ -26,8 +21,9 @@ function btnPress(gather, 10000) {
     score = score-1;         // Increase the score
     scr.edit(score); // Update the page with the new score
   if(score > 15) {
-    hidden.hide(500);
+    //hidden.hide(500);
   }
+}
 //HEAD
 //=======
 function btnPress() {
@@ -35,14 +31,14 @@ function btnPress() {
   scr.edit(score);
 //>>>>>>> upstream/master
 }
-// if(score = 1000){
+//if(score = 1000){
 
- }
+ //}
 //if(gather > 100){
   //hidden.show()
-}
-//if(gather > 100){
-  //hidden.show()
+//}
+if(score > 100){
+  hidden.show()
 }
  function booHide() {
    hidden.hide(1);
